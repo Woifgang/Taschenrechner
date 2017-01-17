4443c1090f9343e7c09f46f0b9507e16958bc173
Woifgang Aschenbrenner <wolfgang.aschenbrenner@gmx.net>
2017-01-17 08:16:21 +0100

    Berechnung und Ausgabe des Ergebnis ausgelagert

--- Taschenrechner/Program.cs
+++ Taschenrechner/Program.cs
@@ -13,45 +13,20 @@ namespace Taschenrechner
             // Benutzereingabe: Als Benutzer möchte ich 2 Zahlen eingeben um deren Ergebnis zu berechnen          
             string ersteZahl = BenutzerEingabe("Geben Sie hier die erste Zahl ein: ");
             string zweiteZahl = BenutzerEingabe("Geben Sie hier die zweite Zahl ein: ");
-            string operand = BenutzerEingabe("Geben sie den Operanden + oder - ein: ");
+            string operand = BenutzerEingabe("Geben sie + , - , * oder / ein: ");
 
             // Todo: Spätere Auslagerung in eine Methode; Umwandlung String -> Double
             double ersteZahlWandlung = Convert.ToDouble(ersteZahl);
             double zweiteZahlWandlung = Convert.ToDouble(zweiteZahl);
 
-            double ergebnis = 0;
+            // Ergebnis berechnen
+            double ergebnis = Berechnung(ersteZahlWandlung, zweiteZahlWandlung, operand);
 
-            // Berechnung
-            switch (operand)
-            {
-                case "+":
-                    ergebnis = Addieren(ersteZahlWandlung, zweiteZahlWandlung);
-                    Console.WriteLine("Das Ergebnis lautet: {0}", ergebnis);
-                    break;
-
-                case "-":
-                    ergebnis = Subtrahieren(ersteZahlWandlung, zweiteZahlWandlung);
-                    Console.WriteLine("Das Ergebnis lautet: {0}", ergebnis);
-                    break;
-
-                case "*":
-                    ergebnis = Multiplizieren(ersteZahlWandlung, zweiteZahlWandlung);
-                    Console.WriteLine("Das Ergebnis lautet: {0}", ergebnis);
-                    break;
-
-                case "/":
-                    ergebnis = Dividieren(ersteZahlWandlung, zweiteZahlWandlung);
-                    Console.WriteLine("Das Ergebnis lautet: {0}", ergebnis);
-                    break;
-                    
-                default:
-                    BenutzerEingabe("Bitte Eingabe prüfen");
-                    break;
-            }
+            // Ergebnis ausgeben
+            Ausgabe(ergebnis, operand);
            
             // Programm beenden
             BenutzerEingabe("Zum Beenden Return drücken");
-            
         }
 
         // Methode: Benutzerdefinierten Text ausgeben und Zahl als String aus Konsole holen
@@ -89,6 +64,63 @@ namespace Taschenrechner
         {
             double ergebnnis = ersteZahl / zweiteZahl;
             return ergebnnis;
+        }
+
+        // Methode: Berechnung
+        static double Berechnung(double ersteZahlWandlung, double zweiteZahlWandlung, string operand)
+        {
+            double ergebnis = 0;
+
+            switch (operand)
+            {
+                case "+":
+                    ergebnis = Addieren(ersteZahlWandlung, zweiteZahlWandlung);
+                    break;
+
+                case "-":
+                    ergebnis = Subtrahieren(ersteZahlWandlung, zweiteZahlWandlung);
+                    break;
+
+                case "*":
+                    ergebnis = Multiplizieren(ersteZahlWandlung, zweiteZahlWandlung);
+                    break;
+
+                case "/":
+                    ergebnis = Dividieren(ersteZahlWandlung, zweiteZahlWandlung);
+                    break;
+
+                default:
+                    ergebnis = 0;
+                    break;             
+            }
+            return ergebnis;
+        }
+
+        // Methode: Ausgabe
+        static void Ausgabe(double ergebnis, string operand)
+        {
+            switch (operand)
+            {
+                case "+":
+                    Console.WriteLine("Das Ergebnis lautet: {0}", ergebnis);
+                    break;
+
+                case "-":
+                    Console.WriteLine("Das Ergebnis lautet: {0}", ergebnis);
+                    break;
+
+                case "*":
+                     Console.WriteLine("Das Ergebnis lautet: {0}", ergebnis);
+                    break;
+
+                case "/":
+                    Console.WriteLine("Das Ergebnis lautet: {0}", ergebnis);
+                    break;
+
+                default:
+                    Console.WriteLine("Falsche Operation eingeben, es ist nur + - * / möglich");
+                    break;
+            }
         }
     }
 }
